083736b38e97114dfc19743c4508fae4a500926c
John <j.hatvani@gmail.com>
2013-03-24 13:15:47 +1100

    Initial xcodeproj and structure

---
+++ JATableCellFactory/JATableCellFactory/JAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JAAppDelegate.h
+//  JATableCellFactory
+//
+//  Created by John Hatvani on 24/03/13.
+//  Copyright (c) 2013 electric-mind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
